c65b2cc49fb0a012b82116bc44076f65caa3e4c1
Olivier Robin <olivier.robin@axa-tech.com>
2016-06-22 20:26:34 +0200

    Tag Site de production

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $ip = $_SERVER['REMOTE_ADDR']; // Recuperation de l'IP du visiteur
 $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip)); //connection au serveur de ip-api.com et recuperation des données
 if($query && $query['status'] == 'success') 
 {
-    echo "Site de Production"<br />\n";
+    echo "Site de Production<br />\n";
 	//code avec les variables
 	echo "Adresse IP = " . $ip ."<br />\n";
 	echo "Pays = " . $query['country'] . "<br />\n";
