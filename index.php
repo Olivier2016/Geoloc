a80de2f0ee145dcbfd6820a2d0a75c1e2f598388
Olivier Robin <olivier.robin@axa-tech.com>
2016-06-22 20:11:13 +0200

    Commit initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR']; // Recuperation de l'IP du visiteur
+$query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip)); //connection au serveur de ip-api.com et recuperation des données
+if($query && $query['status'] == 'success') 
+{
+    //code avec les variables
+	echo "Adresse IP = " . $ip ."<br />\n";
+	echo "Pays = " . $query['country'] . "<br />\n";
+	echo "Region = " . $query['regionName'] . "<br />\n";
+	echo "Ville = " . $query['city'] . "<br />\n";
+	echo "Longitude = " . $query['lon'] . "<br />\n";
+	echo "Latitude = " . $query['lat'] . "<br />\n";
+	echo "ISP = " . $query['isp'] . "<br />\n";
+}
+?>
