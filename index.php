439240adbb20686bbaa320cbf75970099946fdfd
Olivier2016 <olivier.robin@axa-tech.com>
2016-06-24 14:40:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR']; // Recuperation de l'IP du visiteur
-$query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip)); //connection au serveur de ip-api.com et recuperation des données
+$query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip)); //connection au serveur de ip-api.com et recuperation des donnÃ©es
 if($query && $query['status'] == 'success') 
 {
-    echo "Site de Production<br />\n";
+    echo "Site de Production 2<br />\n";
 	//code avec les variables
 	echo "Adresse IP = " . $ip ."<br />\n";
 	echo "Pays = " . $query['country'] . "<br />\n";
